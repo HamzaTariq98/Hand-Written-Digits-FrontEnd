ee69be8339640210df2d43045f53fd70a544b26e
HamzaTariq98 <hamzatarq98@gmail.com>
2024-05-09 19:24:42 +0300

    nodejs

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(process.cwd()+"/dist"));
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
